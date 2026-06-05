8d38f71e738d9e487327ad3a5fd0650ba52905b8
William <131878264+abreuwilliam@users.noreply.github.com>
2026-06-05 15:48:37 -0300

    criando view agendamento

--- src/services/agendamento_service.py
+++ src/services/agendamento_service.py
@@ -1,1 +1,44 @@
 from src import db
+from ..models.agendamento_model import AgendamentoModel
+from ..schemas.agendamento_schema import ServicoSchema
+from flask import jsonify
+
+def cadastrar_agendamento(agendamento):
+    agendamento_db = AgendamentoModel(
+        data_hora=agendamento.data_hora,
+        servico_id=agendamento.servico_id,
+        usuario_id=agendamento.usuario_id,
+        status=agendamento.status
+    )
+    db.session.add(agendamento_db)
+    db.session.commit()
+    return agendamento_db
+
+def listar_agendamento():
+    return AgendamentoModel.query.all() 
+
+def listar_agendamento_id(id):
+    agendamento_encontrado = AgendamentoModel.query.get(id)
+    return agendamento_encontrado
+
+def deletar_agendamento(id):
+    agendamento = AgendamentoModel.query.get(id)
+    if agendamento:
+        db.session.delete(agendamento)
+        db.session.commit()
+        return True
+    return False
+
+def editar_agendamento(id, novo_agendamento):
+    agendamento = AgendamentoModel.query.get(id)
+    if agendamento:
+        agendamento.data_hora = novo_agendamento['data_hora']
+        agendamento.servico_id = novo_agendamento['servico_id']
+        agendamento.usuario_id = novo_agendamento['usuario_id']
+        agendamento.status = novo_agendamento['status']
+
+        db.session.commit()
+        return agendamento
+    return None 
+def listar_agendamento_usuario(usuario_id):
+    return AgendamentoModel.query.filter_by(usuario_id=usuario_id).all()    
---
+++ src/views/agendamento_view.py
@@ -0,0 +1,73 @@
+from flask_restful import Resource
+from marshmallow import ValidationError
+from flask import request, jsonify, make_response
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from src.schemas import agendamento_schema
+from src.models.agendamento_model import AgendamentoModel
+from src.services import agendamento_service
+from src import api
+
+class AgendamentoList(Resource):
+    @jwt_required()
+    def get(self):
+        """
+        Lista todos os agendamentos
+        ---
+        tags: [Agendamento]
+        security: [{Bearer: []}]
+        responses:
+          200: {description: Lista retornada com sucesso}
+        """
+        agendamentos = agendamento_service.listar_agendamento()
+        schema = agendamento_schema.AgendamentoSchema(many=True)
+        return make_response(jsonify(schema.dump(agendamentos)), 200)
+
+    @jwt_required()
+    def post(self):
+        """
+        Cria um novo agendamento
+        ---
+        tags: [Agendamento]
+        security: [{Bearer: []}]
+        parameters:
+          - in: body
+            name: body
+            schema:
+              type: object
+              properties:
+                data: {type: string}
+                servico_id: {type: integer}
+        responses:
+          201: {description: Agendamento criado}
+        """
+        schema = agendamento_schema.AgendamentoSchema()
+        try:
+            dados = schema.load(request.json)
+            # Adiciona o usuário logado ao agendamento se necessário
+            usuario_id = get_jwt_identity()
+            dados['usuario_id'] = usuario_id
+            
+            resultado = agendamento_service.cadastrar_agendamento(dados)
+            return make_response(jsonify(schema.dump(resultado)), 201)
+        except ValidationError as err:
+            return make_response(jsonify(err.messages), 400)
+
+class AgendamentoResource(Resource):
+    @jwt_required()
+    def get(self, id_agendamento):
+        agendamento = agendamento_service.listar_agendamento_id(id_agendamento)
+        if not agendamento:
+            return make_response(jsonify({'message': 'Não encontrado'}), 404)
+        
+        schema = agendamento_schema.AgendamentoSchema()
+        return make_response(jsonify(schema.dump(agendamento)), 200)
+
+    @jwt_required()
+    def delete(self, id_agendamento):
+        if agendamento_service.deletar_agendamento(id_agendamento):
+            return make_response(jsonify({'message': 'Deletado com sucesso'}), 200)
+        return make_response(jsonify({'message': 'Não encontrado'}), 404)
+
+# Registro das rotas
+api.add_resource(AgendamentoList, '/agendamentos')
+api.add_resource(AgendamentoResource, '/agendamentos/<int:id_agendamento>')
